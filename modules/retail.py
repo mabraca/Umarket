083a160ebeeac1d5d9faf250db483c79b9efe455
Javier Moreno <javm12737@gmail.com>
2019-04-23 03:18:16 +0000

    inicio de inventario

--- modules/retail.py
+++ modules/retail.py
@@ -20,40 +20,25 @@ def registerBussines():
         retail.strcorreo=_json['stremail']
         retail.strtelefono=_json['strphone']
         retail.id_tipo=_json['id_type']"""
-        
-        print(request.files.getlist("file[]"))
-        fileRegistroMercantil= request.files.get('filemerchant_register')
-        fileRif= request.files.get('filerif')
-        fileCi= request.files.get('fileci')
-        fileReciboServicio= request.files.get('filereceipt_services')
-        
-        print("paso")
-        print(request.files)
-        #print(fileRegistroMercantil.filename)
-        if not  fileRegistroMercantil:
-            resp = jsonify({"status":'error', "msj":"Debe adjuntar el registro mercantil"})            
-            return sendResponse(resp)
-        print(" ")
-        print(fileRegistroMercantil)
-        #resp = jsonify({"status":'success', "msj":"OK ->"+fileRegistroMercantil.filename})            
-        # return sendResponse(resp)
+       
+        print(request.files)       
+        fileRegistroMercantil=request.files['file[0]']
+        fileRif=request.files['file[1]']
+        fileCi=request.files['file[2]']
+        fileReciboServicio=request.files['file[3]']        
 
-        if  fileRegistroMercantil:      
-            if not fileRegistroMercantil.filename.split('.')[1]=='pdf':
-                resp = jsonify({"status":'error', "msj":"Debe adjuntar un archivo en formato .pdf"})
-                return sendResponse(resp)   
+        print(fileRegistroMercantil.filename.split('.')[1])
         
         folder_documents=time.strftime("%Y%m%d")
         ruta=app.config['UPLOAD_FOLDER']+folder_documents
-        #print(ruta)
+
         if not os.path.exists(ruta):                 
             os.makedirs(app.config['UPLOAD_FOLDER']+folder_documents)
             path_folder_documents=app.config['UPLOAD_FOLDER']+folder_documents
         else: 
             print("existe 02/05/19")
         
-        #print(time.strftime("%Y%m%d"))
-        #print(os.path.exists(time.strftime("%Y%m%d")))
+
 
         # validate the received values
         if request.method == 'POST':           
@@ -86,59 +71,68 @@ def registerBussines():
                 return sendResponse(resp)"""
             
             if  fileRegistroMercantil:            
-                if not fileRegistroMercantil.filename.split('.')=='pdf':
+                if not fileRegistroMercantil.filename.split('.')[1]=='pdf':
                     resp = jsonify({"status":'error', "msj":"El registro mercantil de estar en formato .pdf"})
                     return sendResponse(resp) 
             else:
                 resp = jsonify({"status":'error', "msj":"Debe adjuntar el Registro Mercantil"})
                 return sendResponse(resp)
+            print("fileRegistroMercantil")
             
-            if  fileRifs:
-                if not fileRifs.filename.split('.')=='pdf':
+            if  fileRif:
+                if not fileRif.filename.split('.')[1]=='pdf':
                     resp = jsonify({"status":'error', "msj":"El R.I.F debe estar en formato .pdf"})
                     return sendResponse(resp) 
             else:
                 resp = jsonify({"status":'error', "msj":"Debe adjuntar el R.I.F"})
                 return sendResponse(resp)
+            print("fileRif")
             
             if  fileCi:
-                if not fileCi.filename.slipt('.')=='pdf':
+                if not fileCi.filename.split('.')[1]=='pdf':
                     resp = jsonify({"status":'error', "msj":"La Cédula de Identidad debe estar en formato .pdf"})
                     return sendResponse(resp) 
             else:
                 resp = jsonify({"status":'error', "msj":"Debe adjuntar la Cédula de Identidad"})
                 return sendResponse(resp)
+            print("fileCi")
 
             if  fileReciboServicio:
-                if not fileReciboServicio.filename.split('.')=='pdf':
+                if not fileReciboServicio.filename.split('.')[1]=='pdf':
                     resp = jsonify({"status":'error', "msj":"El Reccibo de servicios debe estar en formato .pdf"})
                     return sendResponse(resp) 
             else:
                 resp = jsonify({"status":'error', "msj":"Debe adjuntar el Recibo de Servicios"})
                 return sendResponse(resp)
+            print("File recibo servicios")
             
-            existe_company=retail.existeCompany(retail.strrif_empresa) 
-            existe_email=retail.existeEmail(retail.strcorreo)            
-            if  existe_company:
+            #existe_company=retail.existeCompany(retail.strrif_empresa) 
+            #existe_email=retail.existeEmail(retail.strcorreo)            
+            """if  existe_company:
                 resp = jsonify({"status":'error', "msj":"El negocio se encuentra registrado"})
-                return sendResponse(resp)
-
-            if existe_email:
+                return sendResponse(resp)"""
+            existe_email=1
+            if not  existe_email:
                 resp = jsonify({"status":'error', "msj":"El correo se encuentra registrado"})
                 return sendResponse(resp)
             else:
-                comercio=retail.registerCompany()
+                #comercio=retail.registerCompany()
                # print(comercio)
+                comercio=1
                 if comercio:                    
-                    retail.id_empresa=comercio['id_empresa']
+                    #retail.id_empresa=comercio['id_empresa']
+                    retail.id_empresa=1
                     ruta_comercio=path_folder_documents+"-"+folder_documents+retail.id_empresa
                     if not os.path.exists(ruta_comercio):
                         os.makedirs(ruta_comercio)
+                    else:
+                        print("existe")
+                    print(ruta_comercio)
 
                     filename_regmercantil="RM_"+secure_filename(fileRegistro_mercantil.filename)+retail.id_empresa
                     fileRegistroMercantil.save(os.path.join(ruta_comercio,filename_regmercantil))          
 
-                    documentosRM=retail.registerDocumentsCompany(ruta_comercio+filename_regmercantil,retail.id_empresa,3)
+                    #documentosRM=retail.registerDocumentsCompany(ruta_comercio+filename_regmercantil,retail.id_empresa,3)
 
                     filename_rif="RIF"+secure_filename(fileRif.filename)+retail.id_empresa
                     fileRif.save(os.path.join(ruta_comercio,filename_rif))
@@ -153,10 +147,10 @@ def registerBussines():
                     filename_rs= "RS"+secure_filename(fileReciboServicio)+retail.id_empresa
                     fileReciboServicio.save(os.path._join(ruta_comercio,filename_rs))
 
-                    documentosRS=retail.registerDocumentsCompany(ruta_comercio+filename_rs,retail.id_empresa,6)
-
-                    if documentosRM and documentosRIF and documentosCI and documentosRS:
-                        resp = jsonify({"status":'success', "msj":"El negocio fue registrado"})
+                    #documentosRS=retail.registerDocumentsCompany(ruta_comercio+filename_rs,retail.id_empresa,6)
+                    if not filename_rs:
+                    #if documentosRM and documentosRIF and documentosCI and documentosRS:"""
+                        resp = jsonify({"status":'success', "msj":"El negocio no fue registrado"})
                         return sendResponse(resp)
                     else:
                         resp = jsonify({"status":'success', "msj":"El negocio fue registrado"})
